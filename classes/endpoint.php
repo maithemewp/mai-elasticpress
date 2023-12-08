111288cac46d1d803131b7457e1eb092a9b91a9b
JiveDig <jivetheuniversal@gmail.com>
2023-12-08 15:04:53 -0500

    add v1 to endpoint and some cleanup

--- classes/endpoint.php
+++ classes/endpoint.php
@@ -40,15 +40,15 @@ class Mai_Elasticpress_Autosuggest_Endpoint {
 	/**
 	 * Register Elasticpress Autosuggest Endpoint.
 	 * You have to specify via Elasticpress settings or `EP_AUTOSUGGEST_ENDPOINT` constant like this:
-	 * @link https://yourdomain.com/wp-json/mai-elasticpress/autosuggest/
+	 * @link https://yourdomain.com/wp-json/mai-elasticpress/v1/autosuggest/
 	 *
 	 * @since 0.8.0
 	 *
 	 * @return void
 	 */
 	function register_endpoint() {
-		register_rest_route( 'mai-elasticpress', '/autosuggest/', [
-			'methods'             => \WP_REST_Server::CREATABLE,
+		register_rest_route( 'mai-elasticpress/v1', '/autosuggest/', [
+			'methods'             => WP_REST_Server::CREATABLE,
 			'callback'            => [ $this, 'callback' ],
 			'permission_callback' => '__return_true',
 		]);
@@ -67,12 +67,11 @@ class Mai_Elasticpress_Autosuggest_Endpoint {
 	 *
 	 * @return array|callable
 	 */
-	function callback( WP_REST_Request $data ){
+	function callback( WP_REST_Request $data ) {
 		$client = ClientBuilder::create();
 		$client->setHosts( [ ElasticPress\Utils\get_host() ] );
 		$client = $client->build();
-
-		$json_params = $data->get_json_params();
+		$json   = $data->get_json_params();
 
 		$params = [
 			'index' => Indexables::factory()->get( 'post' )->get_index_name(),
