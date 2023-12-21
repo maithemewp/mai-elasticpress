78253b6c5b3bf6900e62955e586af51b33c91e08
JiveDig <jivetheuniversal@gmail.com>
2023-12-21 12:56:21 -0500

    aspect-ratio on placeholder

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => '627afb0dddab44ba3fe5');
+<?php return array('dependencies' => array('wp-hooks'), 'version' => '0bece4f843b1faf7a024');
